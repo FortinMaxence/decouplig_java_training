979788d409667ade14e6e2007c8e8b240ea67b5a
Maxence Fortin <fortin@et.esiea.fr>
2021-12-01 21:36:22 +0100

    Part 7

---
+++ src/main/java/fr/lernejo/logger/FilteredLogger.java
@@ -0,0 +1,18 @@
+package fr.lernejo.logger;
+
+import java.util.function.Predicate;
+
+public class FilteredLogger implements Logger{
+    private final Logger delegate;
+    private final Predicate<String> condition;
+    public FilteredLogger(Logger delegate, Predicate<String> condition) {
+        //TODO assign arguments to fields
+        this.delegate = delegate;
+        this.condition = condition;
+    }
+
+    public void log(String message) {
+        if (this.condition.test(message))
+            delegate.log(message);
+    }
+}
--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,7 +2,8 @@ package fr.lernejo.logger;
 
 public class LoggerFactory{
     public static Logger getLogger(String name){
-        return new CompositeLogger(new ContextualLogger(name, new FileLogger("log.txt")),
-            new ContextualLogger(name, new ConsoleLogger()));
+        return new CompositeLogger(
+            new ContextualLogger(name, new FilteredLogger(new FileLogger("log.txt"),
+            message -> !message.contains("player"))), new ContextualLogger(name, new ConsoleLogger()));
     }
 }
